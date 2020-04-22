ef95959868fb240f97c9eb3e340e7ebfa0d32740
John Shearer <john@ultranaut.com>
2020-04-22 00:20:16 -0400

    Objects and context

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,23 @@
+/* eslint prefer-reflect: 0 */
+
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    const rValue = [];
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        rValue.push(`${key}: ${obj[key]}`);
+      }
+    }
+    return rValue;
   }
 };
