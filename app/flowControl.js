f4138c2fbf817f1187727215e4f707db84e62e3c
John Shearer <john@ultranaut.com>
2020-04-21 20:42:39 -0400

    Flow control

--- app/flowControl.js
+++ app/flowControl.js
@@ -11,5 +11,11 @@ exports.flowControlAnswers = {
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
 
+    if (isNaN(num)) { return false; }
+
+    let rvalue = '';
+    if (num % 3 === 0) { rvalue += 'fizz'; }
+    if (num % 5 === 0) { rvalue += 'buzz'; }
+    return rvalue === '' ? num : rvalue;
   }
 };
