a848e0604240589adb193712de3eedf39dba0479
John Shearer <john@ultranaut.com>
2020-04-23 13:00:01 -0400

    Counter

--- app/count.js
+++ app/count.js
@@ -2,6 +2,20 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let counter = start;
+    let timeoutId;
 
+    const doCount = () => {
+      console.log(counter);
+      if (counter++ < end) {
+        timeoutId = setTimeout(doCount, 100);
+      }
+    };
+
+    doCount();
+
+    return {
+      'cancel': () => timeoutId && clearTimeout(timeoutId)
+    };
   }
 };
