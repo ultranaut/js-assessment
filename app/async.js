8b6e0c034997cf802398368998253f678c69fe68
John Shearer <john@ultranaut.com>
2020-04-23 23:21:32 -0400

    Async behavior

--- app/async.js
+++ app/async.js
@@ -2,10 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise((resolve, reject) => {
+      setTimeout(() => resolve(value), 100);
+    });
   },
 
   manipulateRemoteData: function(url) {
+    const parseData = ({people}) => {
+      const names = people.map((person) => person.name);
+      return names.sort();
+    };
 
+    return fetch(url).
+      then((response) => response.json()).
+      then(parseData);
   }
 };
