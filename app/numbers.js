98352c00b9deb97d7ef4910cb77df8f5d93d5465
John Shearer <john@ultranaut.com>
2020-04-25 19:35:06 -0400

    Binary numbers
    Skipped decimal

--- app/numbers.js
+++ app/numbers.js
@@ -1,16 +1,22 @@
+/* eslint no-bitwise:0 */
+
 exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return num >> (bit - 1) & 1;
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    const binary = [];
+    for (let i = 0; i < 8; i++) {
+      binary.unshift(num >> i & 1);
+    }
+    return binary.join('');
   },
 
   multiply: function(a, b) {
